1a528f2fc04de91c37d8cf1fd0b8c08499c8cc92
Timothy Truong, Jr <timt51@live.com>
2013-07-12 15:47:43 -0400

    Added border and edit functionality

--- app.rb
+++ app.rb
@@ -23,6 +23,20 @@ class IdeaBoxApp < Sinatra::Base
     redirect '/'
   end
 
+  get '/:id/edit' do |id|
+    idea = Idea.find(id.to_i)
+    erb :edit, locals: {id: id, idea: idea}
+  end
+
+  put '/:id' do |id|
+    data = {
+      :title => params['idea_title'],
+      :description => params['idea_description']
+    }
+    Idea.update(id.to_i, data)
+    redirect '/'
+  end
+
   not_found do
     erb :error
   end
--- idea.rb
+++ idea.rb
@@ -17,6 +17,17 @@ class Idea
     Idea.database
   end
 
+	def self.find(id)
+	  raw_idea = find_raw_idea(id)
+	  new(raw_idea[:title], raw_idea[:description])
+	end
+
+	def self.find_raw_idea(id)
+	  database.transaction do
+	    database['ideas'].at(id)
+	  end
+	end
+
 	def self.all
 	  raw_ideas.map do |data|
 	    Idea.new(data[:title], data[:description])
@@ -38,4 +49,10 @@ class Idea
       database['ideas'].delete_at(position)
     end
   end
+
+  def self.update(id, data)
+	  database.transaction do
+	    database['ideas'][id] = data
+	  end
+	end
 end
